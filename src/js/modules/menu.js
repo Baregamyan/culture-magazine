46ed5b9d92f9e1840e9d5bf88c3da9e41100b879
Arsen <baregamyan@gmail.com>
2020-01-23 11:21:10 +0300

    Пред-финальная версия

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -2,7 +2,7 @@
  * Находим переключатели меню сайта
  */
 function setupMenusToggling() {
-  const menu = "menu";
+  const menu = 'menu';
   let menus = document.querySelectorAll(`.${menu}`);
   let menusOpen = document.querySelector('.nav__open');
   let menusClose = document.querySelector('.nav__close');
@@ -13,13 +13,13 @@ function setupMenusToggling() {
   menusClose.classList.add('nav__close--hide');
   menusOpen.classList.add('nav__open--show');
   menus.forEach(menu => {
-    menu.classList.remove("menu--no-js");
+    menu.classList.remove('menu--no-js');
   });
 
   menusOpen.addEventListener('click', () => {
     menusOpen.classList.remove('nav__open--show');
     menus.forEach(menu => {
-      menu.classList.add(`menu--show`);
+      menu.classList.add('menu--show');
     });
     menusClose.classList.remove('nav__close--hide');
   })
@@ -27,7 +27,7 @@ function setupMenusToggling() {
   menusClose.addEventListener('click', ()=> {
     menusClose.classList.add('nav__close--hide');
     menus.forEach(menu => {
-      menu.classList.remove(`menu--show`);
+      menu.classList.remove('menu--show');
     });
     menusOpen.classList.add('nav__open--show');
   })
